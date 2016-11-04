eb1adaf9fee086b6dbe5d8a9e41c6e82af6558ae
8simsim8 <8simsim8@gmail.com>
2016-11-04 22:03:18 +0200

    "Правка маркет"

--- www/js/script-flea-market.js
+++ www/js/script-flea-market.js
@@ -1,9 +1,34 @@
-(function () {
+;document.addEventListener("DOMContentLoaded",  market);
 
+function market() {
   workForm('filter');
 
   var $tags = $('.wrap-tags');
-  cropTag($tags);
+  cropTag($tags);					// Обрезание лишних тегов
+  
+  // Анимация кнопки "Избранное"
+  var $favorite = $('.b-ad__item-fav');
+  var strAddFav = $('.b-ad__item-fav').attr('data-add-message');
+  var strRemFav = $('.b-ad__item-fav').attr('data-remove-message');
+
+  $favorite.on('click', function(){
+	  var $this = $(this);
+	  if(!$(this).hasClass('add-to-favorite')) {
+		$(this).attr('data-message', strAddFav);
+		$(this).addClass('add-to-favorite');
+		$(this).addClass('show-text-favorite');
+	  }
+	  else {
+		$(this).attr('data-message', strRemFav);
+		$(this).removeClass('add-to-favorite');
+	  }
+	  $(this).addClass('show-text-favorite');
+	  setTimeout(
+		function(){
+		  $this.removeClass('show-text-favorite');
+		},
+	  1000);
+  });
 
   // Сокращение текста с блоке с новостями
     $("h5[data-clamp]").each(function(index, el){
@@ -12,7 +37,7 @@
     $("p.text[data-clamp]").each(function(index, el){
       $clamp(el, {clamp: 3});
     });
-})();
+};
 
 
 
